649b58a15a2d26c837f24a8569346be2cafc12a4
Mthokozisi Hector Hlatshwayo <mthova.hp@gmail.com>
2024-12-17 13:42:49 +0200

    new updates

--- js/main.js
+++ js/main.js
@@ -61,5 +61,5 @@ $(window).on('load', function () {
       $('#preloader').fadeOut('slow', function () {
           $(this).remove(); // Remove the preloader from the DOM
       });
-  }, 2000); // 3000ms = 3 seconds
+  }, 1000); // 3000ms = 3 seconds
 });
